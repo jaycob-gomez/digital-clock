9e35a99171b4f4b52de7f28db1dfdfdbf5aa0475
jaycob-gomez <143868919+jaycob-gomez@users.noreply.github.com>
2026-03-07 17:52:15 -0800

    add: digital clock functionality using the Date object, an update function, and setInterval to dynamically update the UI

---
+++ README.md
@@ -0,0 +1,13 @@
+# Digital Clock
+
+## Features
+
+- Display current time
+- Update every second
+
+## Concepts used
+
+- Functions
+- setInterval to continuously update the DOM
+- DOM updates
+- Using built-in Date object
---
+++ clock.js
@@ -0,0 +1,33 @@
+const clock = document.querySelector(".clock");
+const span = document.querySelector(".milliseconds");
+
+function updateTime() {
+  // Get the current date and time
+  const now = new Date();
+  console.log(now);
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let milliseconds = now.getMilliseconds();
+
+  console.log(hours, minutes, seconds);
+
+  // Add leading zero to single digit values
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  milliseconds = Math.floor(milliseconds / 10);
+  milliseconds = milliseconds < 10 ? "0" + milliseconds : milliseconds;
+
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  clock.textContent = timeString;
+  span.textContent = milliseconds;
+}
+
+// Update the dom every millisecond
+setInterval(updateTime, 1);
+
+// Initial call displays the time immediately upon loading
+updateTime();
